df2cfb6c55f30a21fa6dbfeea4e9c7986cc27658
clear <2010059016@qq.com>
2025-07-16 17:11:59 +0800

    add autoImport

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import WelcomeItem from './WelcomeItem.vue'
 import SupportIcon from './icons/IconSupport.vue'
 </script>
 
--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,27 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import { viteSingleFile } from 'vite-plugin-singlefile'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools(), viteSingleFile()],
+  plugins: [
+    vue(),
+    vueDevTools(),
+    viteSingleFile(),
+    AutoImport({
+      imports: ['vue', 'vue-router', 'pinia'],
+      dts: 'src/auto-imports.d.ts',
+      eslintrc: {
+        enabled: true,
+      },
+    }),
+    Components({
+      dirs: ['src/components'], // 自动导入的组件目录
+      dts: 'src/components.d.ts', // 生成组件类型声明
+    }),
+  ],
   build: {
     cssCodeSplit: false, // 禁止 CSS 代码分割
     assetsInlineLimit: 1024000, // 小于 1MB 的资源自动转 base64（按需调整）
